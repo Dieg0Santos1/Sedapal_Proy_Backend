585014fc3ca41ff4b9ce2c9069c179987dc80ea5
AlexanderSantos <alexanderasa0210@gmail.com>
2025-11-12 11:17:19 -0500

    fix: agregar dominio de produccion a SecurityConfig CORS

--- src/main/java/com/sedapal/config/SecurityConfig.java
+++ src/main/java/com/sedapal/config/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOrigin("http://localhost:5173");
         configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedOrigin("https://segisedapal.programmersa.com");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
         configuration.setAllowCredentials(true);
